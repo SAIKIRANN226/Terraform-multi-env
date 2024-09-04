600e2b34656ae10a72b4f613620f994609351c43
mothukuru saikiran <obscuresai.com>
2024-09-04 19:22:35 +0530

    message

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb" #devops-practice
-  instance_type = lookup(var.instance_type, terraform.workspace)
+  instance_type = lookup(var.instance_type, terraform.workspace) # why we are using lookup function? in a map we can get the value when you pass the key
 
   tags = {
     Name = "HelloTerraform"
