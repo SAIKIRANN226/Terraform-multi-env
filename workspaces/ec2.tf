10d1abe1e53dfb091f96207d64f62614198bfee9
mothukuru saikiran <obscuresai.com>
2024-10-02 12:15:03 +0530

    message

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb" #devops-practice
-  instance_type = lookup(var.instance_type, terraform.workspace) 
-
+  instance_type = lookup(var.instance_type, terraform.workspace)
   tags = {
     Name = "HelloTerraform"
   }
