42c42870732f44f46f58b7aab367c6eb130621d3
mothukuru saikiran <obscuresai.com>
2025-05-02 11:30:12 +0530

    terraform

--- tfvars/variables.tf
+++ tfvars/variables.tf
@@ -5,8 +5,9 @@ variable "instance_names" {
 #     redis = "t2.micro"
 #     mysql = "t3.small"
 #     web = "t2.micro"
-#   } Created only few to save time while applying or destroying
-}
+  }
+  
+  # Created only few to save time while applying or destroying, and why we are commented lines from 3 to 7 ? this is because just to test if we commented obviously terraform will ask for the prompt then we will know that we missed to give tfvar-file, otherwise it will take default variables but now we commented variables so it will ask for the prompt
 
 variable "zone_id" {
   default = "Z104317737D96UJVA7NEF"
