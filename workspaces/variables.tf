b9f8ff547861c620787230a95197f0bbd1a01ee6
mothukuru saikiran <obscuresai.com>
2025-05-02 07:53:16 +0530

    terraform

--- workspaces/variables.tf
+++ workspaces/variables.tf
@@ -1,6 +1,6 @@
 variable "instance_type" {
   default = {
-    dev = "t2.micro"  # if you are in dev then t2.micro
-    prod = "t3.small" # if you are in prod then t3.small
+    dev = "t2.micro"  # If you are in dev then value becomes t2.micro
+    prod = "t3.small" # If you are in prod then value becomes t3.small
   }
 }
