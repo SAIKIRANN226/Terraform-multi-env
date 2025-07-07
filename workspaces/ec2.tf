665f22acee46af593e1fcfe5826de88749156be9
mothukuru saikiran <obscuresai.com>
2025-07-07 21:10:49 +0530

    testing

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb" #devops-practice
+  ami           = "ami-03265a0778a880afb"
   instance_type = lookup(var.instance_type, terraform.workspace)
   tags = {
     Name = "HelloTerraform"
   }
 }
 
-# line 3 ---> When you are using terraform, it has default variable that is "terraform.workspace", so if you are in dev then the value of "terraform.workspace will automatically becomes dev", if you are in prod then the value of "terraform.workspace will automatically becomes prod". so you need to write lookup function, this function works as if you pass key we can get that value. For example below 
+# line 3 ---> When you are using terraform, it has default variable that is "terraform.workspace", so if you are in dev then the value of "terraform.workspace will automatically becomes dev", if you are in prod then the value of "terraform.workspace will automatically becomes prod". so you need to write lookup function, this function works as if you pass a KEY, we can get that value. For example below 
 
 # lookup(map, key) ---> Giving input as map and passing the key below is the example
 # lookup(var.instance_type, terraform.workspace) ---> 1st one is map and another is key
