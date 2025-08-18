2c910ad40248bbef618f04fd418a7b4470ad08cd
mothukuru saikiran <obscuresai.com>
2025-08-18 13:40:13 +0530

    testing

--- tfvars/variables.tf
+++ tfvars/variables.tf
@@ -7,7 +7,7 @@ variable "instance_names" {
 #     web = "t2.micro"
   }
   
-  # Created only few to save time while applying or destroying, and if we comment lines from 3 to 7 ? this is because just to test if we commented, obviously terraform will ask for the prompt then we will know that we missed to give tfvar-file, otherwise it will take default variables but now we commented variables so it will ask for the prompt. 
+# Created only few to save time while applying or destroying, and if we comment lines from 3 to 7 ? this is because just to test if we commented, obviously terraform will ask for the prompt then we will know that we missed to give tfvar-file, otherwise it will take default variables but now we commented variables so it will ask for the prompt. 
 
 variable "zone_id" {
   default = "Z104317737D96UJVA7NEF"
